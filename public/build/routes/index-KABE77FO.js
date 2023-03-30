8a7ff01b49361582c774595648cfc149e4cc5ea7
ikorecky <ikorecky@gmail.com>
2023-03-30 21:25:58 +0300

    revoked remove build dir from branch

--- public/build/routes/index-KABE77FO.js
+++
@@ -1,1 +0,0 @@
-import"/build/_shared/chunk-G5WX4PPA.js";function r(){return null}export{r as default};
--- public/build/routes/wizard-WOKDLRZ7.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"/build/_shared/chunk-6SVPTVQG.js";import{d as e}from"/build/_shared/chunk-G5WX4PPA.js";var t=e(o());function r(){return null}export{r as default};
